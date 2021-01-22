121085add7f670c3a09ed37520e9917c76fc6757
Yu Seungyeon <59113914+seungsang00@users.noreply.github.com>
2021-01-22 18:16:40 +0900

    Pre 10기 유승연 JS Calculator Bare Minimum 제출합니다.

--- script.js
+++ script.js
@@ -9,7 +9,22 @@ const calculatedResult = document.querySelector('.calculator__result'); // calcu
 function calculate(n1, operator, n2) {
   let result = 0;
   // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
+  // 입력값은 문자열 타입
   // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
+  // +, -, *, /
+  let num1 = Number(n1);
+  let num2 = Number(n2);
+
+  if (operator === '+') {
+    result = num1 + num2;
+  } else if (operator === '-') {
+    result = num1 - num2;
+  } else if (operator === '*') {
+    result = num1 * num2;
+  } else if (operator === '/') {
+    result = num1 / num2;
+  }
+  
   return String(result);
 }
 
@@ -21,29 +36,60 @@ buttons.addEventListener('click', function (event) {
   const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
   // ! 위 코드(Line 19 - 21)는 수정하지 마세요.
 
+  let clickedBefore = 'number'; 
+
   if (target.matches('button')) {
     // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
+    
+    
     // 클릭된 HTML 엘리먼트가 button이면
     if (action === 'number') {
       // 그리고 버튼의 클레스가 number이면
       // 아래 코드가 작동됩니다.
+      // 만약 firstOperend의 숫자가 0이면 buttonCotent로 바꿔주자
+      // 만약 firstOperend의 숫자가 0이 아니면 이어붙여주자(일단)
+      if (firstOperend.textContent === '0') {
+        firstOperend.textContent = buttonContent;
+      } else {
+        secondOperend.textContent = buttonContent;
+      }
+      // else if (clickedBefore === 'operator') {
+      //   secondOperend.textContent = buttonContent;
+      // } else if (clickedBefore !== 'operator') {
+      //   firstOperend.textContent = firstOperend.textContent + buttonContent;
+      // } 
       console.log('숫자 ' + buttonContent + ' 버튼');
+      console.log(clickedBefore);
     }
 
     if (action === 'operator') {
+      operator.textContent = buttonContent;
+      clickedBefore = 'operator'; // 클릭된 HTML 엘리먼트의 클래스 정보를 저장
+
       console.log('연산자 ' + buttonContent + ' 버튼');
+      console.log(clickedBefore);
     }
 
     if (action === 'decimal') {
-      // console.log('소수점 버튼');
+      console.log('소수점 버튼');
+      console.log(clickedBefore);
     }
 
     if (action === 'clear') {
+      firstOperend.textContent = '0';
+      operator.textContent = '+';
+      secondOperend.textContent = '0';
+      calculatedResult.textContent = '0';
+      clickedBefore = 'number';
+
       console.log('초기화 버튼');
+      console.log(clickedBefore);
     }
 
     if (action === 'calculate') {
+      calculatedResult.textContent = calculate(firstOperend.textContent, operator.textContent, secondOperend.textContent);
       console.log('계산 버튼');
+      console.log(clickedBefore);
     }
   }
 });
